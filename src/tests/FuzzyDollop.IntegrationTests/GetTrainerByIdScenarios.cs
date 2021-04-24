2ff9a3f95e43c22241688254413e3a7b39b27712
Andrew Hillier <ahillier88@live.com>
2021-04-24 07:39:05 +0200

    Add basic failing test.

---
+++ src/tests/FuzzyDollop.IntegrationTests/GetTrainerByIdScenarios.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using AutoFixture;
+using FluentAssertions;
+using FuzzyDollop.Api;
+using Xbehave;
+using Xunit;
+
+namespace FuzzyDollop.IntegrationTests
+{
+    public class GetTrainerByIdScenarios : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly IFixture _fixture;
+        private readonly HttpClient _httpClient;
+
+        public GetTrainerByIdScenarios(CustomWebApplicationFactory<Startup> factory)
+        {
+            _fixture = new Fixture();
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Scenario]
+        public void TrainerDoesNotExist(Guid trainerId, HttpResponseMessage response)
+        {
+            "Given the trainerId does not match any existing records"
+                .x(() => { trainerId = _fixture.Create<Guid>(); });
+
+            "When the trainer is requested"
+                .x(async () => response = await _httpClient.GetAsync($"trainers/{trainerId}"));
+
+            "Then the response should be 404 Not Found"
+                .x(() => { response.StatusCode.Should().Be(HttpStatusCode.NotFound); });
+        }
+    }
+}
